3ef4567ebda0df1c826d7d4d48a4868bba9bac97
Pedrohmedd <pedromedeirosetec02@gmail.com>
2026-04-07 16:23:31 -0300

    feat: initialize project structure with layout, global styles, and main components

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar"
+import Hero from "@/components/Hero"
+import Projects from "@/components/Projects"
+import Footer from "@/components/Footer"
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Projects />
+      <Footer />
+    </main>
+  )
+}
